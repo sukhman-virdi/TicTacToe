c7cf99ee1c59c6dacec3d5941a354e839f348a04
alexaverga <alexantonioverga@gmail.com>
2026-03-27 11:22:45 -0700

    updated db.php to handle multiple password cases

--- db.php
+++ db.php
@@ -1,7 +1,25 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'root', 'tictactoe');
+
+$host = 'localhost';
+$user = 'root';
+$db   = 'tictactoe';
+
+//List of possible passwords
+$passwords = ['', 'root'];
+
+//test each password and break loop if the connection is successful
+foreach ($passwords as $pass) {
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+    if ($conn) {
+        break;
+    }
+}
 
 if (!$conn) {
-    die(json_encode(['error' => 'Connection failed: ' . mysqli_connect_error()]));
+    die(json_encode([
+        'error' => 'Connection failed: ' . mysqli_connect_error()
+    ]));
 }
+
 ?>
