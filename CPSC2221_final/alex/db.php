ec9dff08e865039276dda3f45ea3f66161cf543e
Tomo <tsakai02@mylangara.ca>
2026-03-29 22:47:34 -0700

    delete files

--- CPSC2221_final/alex/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'tictactoe');
-
-if (!$conn) {
-    die(json_encode(['error' => 'Connection failed: ' . mysqli_connect_error()]));
-}
-?>
--- CPSC2221_final/html/checkRole.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-$allowed = ['player', 'manager'];
-if (!empty($_POST['role']) && in_array($_POST['role'], $allowed)) {
-	$_SESSION['role'] = $_POST['role'];
-}
-?>
